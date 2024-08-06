dc7bb3c0e4d7788c574f20b206b71364186cd617
Deepak Sharma <ce.deepaksharma@gmai.com>
2024-08-06 10:28:57 +0530

    primary and seconday header

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <h1 className="text-center text-3xl font-bold underline">
-      Welcome to Exclusive app
-    </h1>
-  );
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./Home.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import ErrorPage from "./components/error-page.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
